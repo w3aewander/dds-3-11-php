5233e2bb3c782813d8c288f95fb6b01ec7345e9f
Wanderlei Silva WS <wander.silva@labcetec.net>
2022-08-26 17:16:03 -0300

    Primeiro Commit

---
+++ src/public/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Script principal
+ * @author Wanderlei Silva do Carmo <wander.silva@gmail.com>
+ * @version 1.0
+ */
+ 
+ /**
+  *  Comentário em várias linhas...
+  */
+
+ // Comentário em uma
+ // linha...
+ // toda linha que iniciar com  '//' é ignorada pelo compilador
+
+ include __DIR__ .  "/../../vendor/autoload.php";
+
+
+ include __DIR__ . "/../views/index.html";
