3da1550224494dccb09534d9dad86dd81788e46d
Wanderlei Silva WS <wander.silva@labcetec.net>
2022-08-31 17:25:06 -0300

    Adicionado suporte a javascript

---
+++ src/public/js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Mõdulo javascript da aplicação...
+ * @author Wanderlei Silva do Carmo <wander.silva@gmail.com>
+ * @version 1.0
+ * 
+ */
+
+ // Tudo que quiser que seja executado no momento em que
+ // a página é carregada
+ document.addEventListener('DOMContentLoaded', () => {
+      alert('Opa, funfou! Javascript rodando perfeitamente.')
+ })
+
